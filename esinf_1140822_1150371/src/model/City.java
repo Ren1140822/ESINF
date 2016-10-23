375a1cd712c22e5d036b1f443fd4b8d3afcac257
Renato <1140822@isep.ipp.pt>
2016-10-23 17:37:22 +0100

    early version of model classes implemented add class City add classe User [implementation]

---
+++ esinf_1140822_1150371/src/model/City.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Renato Oliveira 1140822@isep.ipp.pt
+ */
+public class City {
+
+    private String cityName;
+
+    private long  latitude;
+    
+    private long longitude;
+
+    private int numberOfPointsAwarded;
+    
+    private User mayor;
+    
+    private static String DEFAULT_CITY_NAME ="No name";
+    
+    private static long DEFAULT_LATITUDE=0;
+    
+      private static long DEFAULT_LONGITUDE=0;
+    
+    private static int DEFAULT_NUMBER_OF_POINTS_AWARDED =0;
+
+    public City() {
+        this.cityName=DEFAULT_CITY_NAME;
+        this.latitude = DEFAULT_LATITUDE;
+        this.longitude =DEFAULT_LONGITUDE;
+        this.numberOfPointsAwarded = DEFAULT_NUMBER_OF_POINTS_AWARDED;  
+    }
+
+    public City(String cityName, long latitude,long longitude, int numberOfPointsAwarded) {
+        this.cityName = cityName;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.numberOfPointsAwarded = numberOfPointsAwarded;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public long getLatitude() {
+        return latitude;
+    }
+
+    public long getLongitude() {
+        return longitude;
+    }
+    
+    
+    
+
+    public int getNumberOfPointsAwarded() {
+        return numberOfPointsAwarded;
+    }
+
+    public User getMayor() {
+        return mayor;
+    }
+
+    
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public void setLatitude(long latitude) {
+        this.latitude =latitude;
+    }
+
+    public void setLongitude(long longitude) {
+        this.longitude = longitude;
+    }
+
+    
+    
+    public void setNumberOfPointsAwarded(int numberOfPointsAwarded) {
+        this.numberOfPointsAwarded = numberOfPointsAwarded;
+    }
+
+    public void setMayor(User mayor) {
+        this.mayor = mayor;
+    }
+    
+    
+
+}
---
+++ esinf_1140822_1150371/src/model/User.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author Renato Oliveira 1140822@isep.ipp.pt
+ */
+public class User {
+    
+    
+    private String nickname;
+    
+    private String email;
+    
+    private String currentCity;
+    
+    private Set<User> friends;
+    
+    private int visitPoints;
+
+    private static String DEFAULT_NICKNAME="No nickname";
+    
+    private static String DEFAULT_EMAIL ="No email";
+    
+    private static String DEFAULT_CURRENT_CITY="No current city";
+    
+    private static int DEFAULT_VISIT_POINTS=0;
+    
+    
+    public User()
+    {
+         this.nickname = DEFAULT_NICKNAME;
+        this.email = DEFAULT_EMAIL;
+        this.currentCity = DEFAULT_CURRENT_CITY;
+        this.friends = new HashSet<User>();
+        this.visitPoints =DEFAULT_VISIT_POINTS;
+        
+        
+    }
+    
+    public User(String nickname, String email, String currentCity, Set<User> friends, int visitPoints) {
+        this.nickname = nickname;
+        this.email = email;
+        this.currentCity = currentCity;
+        this.friends = friends;
+        this.visitPoints = visitPoints;
+    }
+    
+    
+}
