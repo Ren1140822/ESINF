e447f40a02f1c92f94e161da5e5722aa7a391a7a
Renato <1140822@isep.ipp.pt>
2016-10-23 18:43:40 +0100

    changes made to model implementation(equals) Method to read city from file added JUnit test added [implementation]

--- esinf_1140822_1150371/src/model/City.java
+++ esinf_1140822_1150371/src/model/City.java
@@ -13,34 +13,36 @@ public class City {
 
     private String cityName;
 
-    private long  latitude;
-    
+    private long latitude;
+
     private long longitude;
 
     private int numberOfPointsAwarded;
-    
+
     private User mayor;
-    
-    private static String DEFAULT_CITY_NAME ="No name";
-    
-    private static long DEFAULT_LATITUDE=0;
-    
-      private static long DEFAULT_LONGITUDE=0;
-    
-    private static int DEFAULT_NUMBER_OF_POINTS_AWARDED =0;
+
+    private static String DEFAULT_CITY_NAME = "No name";
+
+    private static long DEFAULT_LATITUDE = 0;
+
+    private static long DEFAULT_LONGITUDE = 0;
+
+    private static int DEFAULT_NUMBER_OF_POINTS_AWARDED = 0;
 
     public City() {
-        this.cityName=DEFAULT_CITY_NAME;
+        this.cityName = DEFAULT_CITY_NAME;
         this.latitude = DEFAULT_LATITUDE;
-        this.longitude =DEFAULT_LONGITUDE;
-        this.numberOfPointsAwarded = DEFAULT_NUMBER_OF_POINTS_AWARDED;  
+        this.longitude = DEFAULT_LONGITUDE;
+        this.numberOfPointsAwarded = DEFAULT_NUMBER_OF_POINTS_AWARDED;
+        this.mayor = new User();
     }
 
-    public City(String cityName, long latitude,long longitude, int numberOfPointsAwarded) {
+    public City(String cityName, long latitude, long longitude, int numberOfPointsAwarded) {
         this.cityName = cityName;
         this.latitude = latitude;
         this.longitude = longitude;
         this.numberOfPointsAwarded = numberOfPointsAwarded;
+        this.mayor = new User();
     }
 
     public String getCityName() {
@@ -54,9 +56,6 @@ public class City {
     public long getLongitude() {
         return longitude;
     }
-    
-    
-    
 
     public int getNumberOfPointsAwarded() {
         return numberOfPointsAwarded;
@@ -66,21 +65,18 @@ public class City {
         return mayor;
     }
 
-    
     public void setCityName(String cityName) {
         this.cityName = cityName;
     }
 
     public void setLatitude(long latitude) {
-        this.latitude =latitude;
+        this.latitude = latitude;
     }
 
     public void setLongitude(long longitude) {
         this.longitude = longitude;
     }
 
-    
-    
     public void setNumberOfPointsAwarded(int numberOfPointsAwarded) {
         this.numberOfPointsAwarded = numberOfPointsAwarded;
     }
@@ -88,7 +84,15 @@ public class City {
     public void setMayor(User mayor) {
         this.mayor = mayor;
     }
-    
-    
+
+    public boolean equals(Object otherObject) {
+
+        if(this==otherObject)
+        {
+            return true;
+        }
+        City otherCity = (City)otherObject;
+        return this.cityName.equals(otherCity.cityName) && this.latitude == otherCity.latitude && this.longitude == otherCity.longitude && this.numberOfPointsAwarded == otherCity.numberOfPointsAwarded && this.mayor.equals(otherCity.mayor);
+    }
 
 }
