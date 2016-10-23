de5848b04dfb7af14dbc42b1bd93d66c9f4b9764
Renato <1140822@isep.ipp.pt>
2016-10-23 17:44:16 +0100

    add classe InputOutput method signature created [implementation]

---
+++ esinf_1140822_1150371/src/utils/InputOutput.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ */
+package utils;
+
+import java.util.HashSet;
+import java.util.Set;
+import model.City;
+import model.User;
+
+/**
+ *
+ * @author Renato Oliveira 1140822@isep.ipp.pt
+ */
+public class InputOutput {
+    
+    
+    
+    public static Set<City> readCityFromFile(String filePath)
+    {
+        Set<City> cities = new HashSet<City>();
+        
+        return cities;
+                
+    }
+    
+     public static Set<User> readUsersFromFile(String filePath)
+    {
+        Set<User> users = new HashSet<User>();
+        
+        
+        
+        return users;
+        
+    }
+     
+     
+}
