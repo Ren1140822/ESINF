ce94476e1c64787579daffdfc9daffa9c9f2c7f0
Your Name <1150371@isep.ipp.pt>
2016-10-25 08:39:32 +0100

    remove user

---
+++ esinf_1140822_1150371/src/controller/RemoveUserController.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import model.MainRegistry;
+import model.User;
+
+/**
+ *
+ * @author Jose Silva <1150371@isep.ipp.pt>
+ */
+public class RemoveUserController {
+
+    MainRegistry r;
+
+    public RemoveUserController(MainRegistry r) {
+        this.r = r;
+    }
+
+    public boolean removeUser(String nick) {
+        User user = r.getListOfUsers().getUserByNickname(nick);
+        if (r.getListOfUsers().getListOfUsers().contains(user)) {
+            r.getListOfUsers().getListOfUsers().remove(user);
+            return true;
+        }
+        return false;
+    }
+}
