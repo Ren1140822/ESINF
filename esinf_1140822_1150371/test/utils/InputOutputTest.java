0965d6352a998d97b732ab499593d6daf34100fc
Jose Silva <1150371@isep.ipp.pt>
2016-10-30 20:41:39 +0000

    Teste read user from file com erros

--- esinf_1140822_1150371/test/utils/InputOutputTest.java
+++ esinf_1140822_1150371/test/utils/InputOutputTest.java
@@ -7,6 +7,8 @@ package utils;
 
 import java.io.FileNotFoundException;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import model.City;
@@ -25,23 +27,22 @@ import model.MainRegistry;
  * @author Renato Oliveira 1140822@isep.ipp.pt
  */
 public class InputOutputTest {
-   
-    
+
     public InputOutputTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -53,23 +54,22 @@ public class InputOutputTest {
     public void testReadCityFromFile() throws FileNotFoundException {
         System.out.println("readCityFromFile");
         String filePath = "D:\\Cities.txt";
-        
+
         Set<City> expResult = new HashSet();
-        expResult.add(new City("porto",221,2003.3321,2));
+        expResult.add(new City("porto", 221, 2003.3321, 2));
         Set<City> result = InputOutput.readCityFromFile(filePath);
-        boolean isEqual=false;
-        for(City city : expResult)
-        {
+        boolean isEqual = false;
+        for (City city : expResult) {
             for (City city2 : result) {
-                isEqual= city.equals(city2);
-                
+                isEqual = city.equals(city2);
+
             }
-            if(!isEqual){
+            if (!isEqual) {
                 break;
             }
         }
         assertTrue(isEqual);
-      
+
     }
 
     /**
@@ -82,17 +82,84 @@ public class InputOutputTest {
         String filePath = "D:\\users10.txt";
         MainRegistry r = new MainRegistry();
         Set<City> cities = InputOutput.readCityFromFile(cityPath);
-       ListOfCities listOfCities = new ListOfCities();
-       listOfCities.setListOfCities(cities);
-       r.setListOfCities(listOfCities);
-       Set<User> expResult =  new HashSet();
-       
-      //TODO, criar set utilizadores com expected result, pelo menos 5
-     Set<User> result = InputOutput.readUsersFromFile(filePath, r);
+        ListOfCities listOfCities = new ListOfCities();
+        listOfCities.setListOfCities(cities);
+        r.setListOfCities(listOfCities);
+        Set<User> expResult = new HashSet();
+        expResult.add(nick0(r));
+        expResult.add(nick1(r));
+        expResult.add(nick2(r));
+        expResult.add(nick3(r));
+        expResult.add(nick4(r));
+        Set<User> result = InputOutput.readUsersFromFile(filePath, r);
         assertEquals(expResult, result);
-       
+
     }
 
-  
-    
+    private User nick0(MainRegistry r) {
+        List<City> cities = new LinkedList();
+        cities.add(r.getListOfCities().getCityByName("city4"));
+        cities.add(r.getListOfCities().getCityByName("city6"));
+        Set<User> friends = new HashSet();
+        friends.add(r.getListOfUsers().getUserByNickname("nick7"));
+        friends.add(r.getListOfUsers().getUserByNickname("nick4"));
+        friends.add(r.getListOfUsers().getUserByNickname("nick3"));
+        User u = new User("nick0", "mail_0_@sapo.pt", null, friends, cities, 144);
+        return u;
+    }
+
+    private User nick1(MainRegistry r) {
+        List<City> cities = new LinkedList();
+        cities.add(r.getListOfCities().getCityByName("city5"));
+        cities.add(r.getListOfCities().getCityByName("city9"));
+        cities.add(r.getListOfCities().getCityByName("city4"));
+        Set<User> friends = new HashSet();
+        friends.add(r.getListOfUsers().getUserByNickname("nick6"));
+        friends.add(r.getListOfUsers().getUserByNickname("nick2"));
+        friends.add(r.getListOfUsers().getUserByNickname("nick8"));
+        User u = new User("nick1", "mail_1_@sapo.pt", null, friends, cities, 0);
+        u.setVisitPoints(u.getVisitPoints());
+        return u;
+    }
+
+    private User nick2(MainRegistry r) {
+        List<City> cities = new LinkedList();
+        cities.add(r.getListOfCities().getCityByName("city2"));
+        cities.add(r.getListOfCities().getCityByName("city0"));
+        Set<User> friends = new HashSet();
+        friends.add(r.getListOfUsers().getUserByNickname("nick6"));
+        friends.add(r.getListOfUsers().getUserByNickname("nick7"));
+        friends.add(r.getListOfUsers().getUserByNickname("nick8"));
+        friends.add(r.getListOfUsers().getUserByNickname("nick1"));
+        User u = new User("nick2", "mail_2_@sapo.pt", null, friends, cities, 0);
+        u.setVisitPoints(u.getVisitPoints());
+        return u;
+    }
+
+    private User nick3(MainRegistry r) {
+        List<City> cities = new LinkedList();
+        cities.add(r.getListOfCities().getCityByName("city8"));
+        Set<User> friends = new HashSet();
+        friends.add(r.getListOfUsers().getUserByNickname("nick0"));
+        friends.add(r.getListOfUsers().getUserByNickname("nick8"));
+        friends.add(r.getListOfUsers().getUserByNickname("nick7"));
+        friends.add(r.getListOfUsers().getUserByNickname("nick6"));
+        friends.add(r.getListOfUsers().getUserByNickname("nick5"));
+        User u = new User("nick3", "mail_3_@sapo.pt", null, friends, cities, 0);
+        u.setVisitPoints(u.getVisitPoints());
+        return u;
+    }
+
+    private User nick4(MainRegistry r) {
+        List<City> cities = new LinkedList();
+        cities.add(r.getListOfCities().getCityByName("city6"));
+        cities.add(r.getListOfCities().getCityByName("city8"));
+        Set<User> friends = new HashSet();
+        friends.add(r.getListOfUsers().getUserByNickname("nick9"));
+        friends.add(r.getListOfUsers().getUserByNickname("nick0"));
+        User u = new User("nick4", "mail_4_@sapo.pt", null, friends, cities, 0);
+        u.setVisitPoints(u.getVisitPoints());
+        return u;
+    }
+
 }
